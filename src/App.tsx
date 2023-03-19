b6d22937d54dc26a3b1e33b43134743846c8aed5
Narei <ieranofficial@gmail.com>
2023-03-19 21:52:07 +0100

    drawing v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import AccessibilityBar from "./components/AccessibilityBar";
+import DrawingBoard from "./components/DrawingBoard";
 import ToolsBar from "./components/ToolsBar";
 
 function App() {
+
 	return (
-		<div className="overflow-hidden w-screen h-screen bg-neutral-200 flex flex-col">
+		<div className="w-screen h-screen bg-neutral-200 flex flex-col">
 			{/* Menu, save, changes history */}
-			<AccessibilityBar></AccessibilityBar>
+			<AccessibilityBar ></AccessibilityBar>
 
 			{/* Wrapper */}
 			<div className="flex w-full h-full">
 				{/* Canvas */}
-				<div className="w-full h-full overflow-auto">
-
+				<div className="w-full h-full overflow-auto relative">
+					<DrawingBoard></DrawingBoard>
 				</div>
 
 				{/* Drawing tools */}
---
+++ src/components/DrawingBoard.tsx
@@ -0,0 +1,76 @@
+import { useEffect, useRef, useState } from "react";
+
+function DrawingBoard() {
+    const [ctx, setCtx] = useState<CanvasRenderingContext2D | undefined | null>(null);
+    const [painting, setPainting] = useState({
+        isPainting: false,
+        startX: 0,
+        startY: 0,
+    })
+
+    const canvasElement = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        setCtx(canvasElement.current?.getContext('2d'))
+        if(canvasElement.current) {
+            canvasElement.current.width = canvasElement.current.clientWidth
+            canvasElement.current.height = canvasElement.current.clientHeight
+        }
+    }, [canvasElement])
+
+    const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        e.preventDefault();
+
+        if(ctx && canvasElement.current) {
+            let rect = (e.target as HTMLElement).getBoundingClientRect();
+            let x = e.clientX - rect.left;
+            let y = e.clientY - rect.top;
+            setPainting({
+                isPainting: true,
+                startX: x,
+                startY: y
+            })
+        }
+    }
+
+    const handleMouseUp = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        setPainting({
+            isPainting: false,
+            startX: 0,
+            startY: 0
+        })
+        if(ctx) {
+            ctx.stroke();
+            ctx.beginPath();
+        }
+    }
+
+    const handleDraw = (e: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
+        if(!painting.isPainting) return;
+
+        let rect = (e.target as HTMLElement).getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        if(ctx && canvasElement.current) {
+            ctx.lineWidth = 1;
+            ctx.lineCap = 'round';
+            ctx.lineJoin = 'round';
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
+    }
+
+    return (
+        <canvas ref={canvasElement} 
+                onMouseDown={(e) => handleMouseDown(e)} 
+                onMouseUp={(e) => handleMouseUp(e)}
+                onMouseMove={(e) => handleDraw(e)}
+                className="bg-white absolute w-full h-full cursor-crosshair"
+        >
+
+        </canvas>
+    )
+}
+
+export default DrawingBoard;
